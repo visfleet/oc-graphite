b1aaa5d8ccef3215b649011191ffa230210c2e54
Jonathan Hoskin <jonathan.hoskin@visfleet.com>
2015-08-06 18:15:51 +1200

    Create missing AWS bits

--- recipes/_uwsgi.rb
+++ recipes/_uwsgi.rb
@@ -15,21 +15,32 @@ end
 
 case node[:platform]
 when 'amazon'
+  graphite_web_path = '/var/lib/graphite'
+
+  bash 'copy-uwsgi-carbon-conf' do
+    code "cp #{graphite_web_path}/conf/graphite.wsgi.example #{graphite_web_path}/conf/graphite.wsgi"
+    not_if { ::File.exist?("#{graphite_web_path}/conf/carbon.conf") }
+  end
+
   template '/etc/uwsgi.d/graphite.ini' do
     source 'uwsgi-graphite.erb'
     owner 'root'
     group 'root'
     mode 0644
+    variables({ :graphite_web_path => graphite_web_path })
 
     notifies :reload, 'service[uwsgi]', :delayed
   end
 
 when 'ubuntu'
+  graphite_web_path = '/usr/share/graphite-web'
+
   template '/etc/uwsgi/apps-available/graphite.ini' do
     source 'uwsgi-graphite.erb'
     owner 'root'
     group 'root'
     mode 0644
+    variables({ :graphite_web_path => graphite_web_path })
 
     notifies :reload, 'service[uwsgi]', :delayed
   end
